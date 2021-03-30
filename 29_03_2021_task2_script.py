fc2ef99c042be8d33a51d2580e6fdce995eacc06
maksymefimov <81595997+maksymefimov@users.noreply.github.com>
2021-03-30 20:17:38 +0300

    Add files via upload

---
+++ 29_03_2021_task2_script.py
@@ -0,0 +1,8 @@
+name=input("what is your name??? ")
+age=input("How old are you??? ")
+country=input("Where are you live??? ")
+print()
+print("Hello, ",name,"!")
+print("Your age is ",age,".")
+print("You live in ",country,".")
+    
