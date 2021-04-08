16117c155ad75aefd17b8e8fbfa2689f4d2bd3fd
maksymefimov <81595997+maksymefimov@users.noreply.github.com>
2021-04-09 00:27:13 +0300

    STUDY HARD

---
+++ mylearning/topic5/task9.py
@@ -0,0 +1,18 @@
+counter = 1
+positive = 0
+negative = 0 
+while counter != 0:
+    num = int(input("Input number:"))
+    if num == 0:
+        break
+    if num > 0:
+        positive += 1
+    elif num < 0:
+        negative += 1
+    procent100 = 100 / (positive + negative)
+    procent_positive = procent100 * positive
+    procent_negative = procent100 * negative
+    print("Percentage of positive numbers: %d%%" % procent_positive)
+    print("Percentage of positive numbers: %d%%" % procent_negative)
+print("The End")    
+       
