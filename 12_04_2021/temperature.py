b8881905f6ce7a132eb9529550d74de2a6fb72b3
maksymefimov <81595997+maksymefimov@users.noreply.github.com>
2021-04-12 23:04:42 +0300

    refactoring

--- 12_04_2021/temperature.py
+++ 12_04_2021/temperature.py
@@ -1,11 +1,11 @@
 list_month = ["","January","February","March","April","May","June","July",\
               "August","September","October","November","December"]
 month = 1
-maxx = float(input("Input Temperature in %s: "%list_month[month]))
+max_temp = float(input("Input Temperature in %s: "%list_month[month]))
 for i in range(2,13):
     temp = float(input("Input Temperature in %s: "%list_month[i]))
-    if temp > maxx:
-        maxx = temp
+    if temp > max_temp:
+        max_temp = temp
         month = i
-print("max temp = %.1f in %s month"%(maxx,list_month[month]))
+print("Max. temperature = %.1f in %s month"%(max_temp,list_month[month]))
     
