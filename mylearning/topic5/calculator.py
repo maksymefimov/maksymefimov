22397dc832ede70f59d2502ed717c17ab3b0dfc8
maksymefimov <81595997+maksymefimov@users.noreply.github.com>
2021-04-12 22:45:24 +0300

    tasks

--- mylearning/topic5/calculator.py
+++ mylearning/topic5/calculator.py
@@ -1,13 +1,13 @@
-#повідомити користувача про умови обчислень
+
 print ("hello i m calculator")
 print("You_ve to input operation sign: +,-,*,/")
 print("Input 0 to stop!")
 sign = input("Input sign please: ")
 while sign not in '0+-*/ ':
         sign = input("Input sign please: ")
-#цикл для введення операцій
+
 while sign != '0':
-    #перевірка яка дія та її виконання
+   
     a = int(input("Input a: "))
     b = int(input("Input b: "))
     if sign=='+':
--- mylearning/topic5/task1.py
+++ mylearning/topic5/task1.py
@@ -1,5 +1,5 @@
-print("%11s"%"Table")
-print("%0s %5s %5s %5s" % ("x","x^2","x^3","x^4"))
+print("%15s"%"Table")
+print("%4s %6s %6s %6s" % ("x","x^2","x^3","x^4"))
 for i in range(1,11):
-    print("%0s %5s %5s %5s" % (i**1,i**2,i**3,i**4))
+    print("%4d %6d %6d %6d" % (i**1,i**2,i**3,i**4))
 
