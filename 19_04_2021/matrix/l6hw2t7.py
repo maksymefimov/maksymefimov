ea3554c7d4f599674ef06c8918701b2512c0add2
maksymefimov <81595997+maksymefimov@users.noreply.github.com>
2021-04-19 23:55:13 +0300

    rebuild

--- 19_04_2021/matrix/l6hw2t7.py
+++ 19_04_2021/matrix/l6hw2t7.py
@@ -1,22 +1,22 @@
 from random import randint
 matrix = [[randint(0,99) for j in range(5)] for i in range(5)]
-
+print(type(matrix))
 print("Original matrix:")
 for i in range(5):
     for j in range(5):
         print("%4d"%matrix[i][j], end = '')
     print()
-counter_1 = 0 
-while counter_1 < 5:
-    counter_2 = 0
-    for j in range(counter_1+1):
-        if matrix[0][j] > matrix[0][counter_2]:
-            counte_2 = j
+counter = 4
+while counter > 0:
+    verification = 0
+    for j in range(counter+1):
+        if matrix[0][j] > matrix[0][verification]:
+            verification = j
     for i in range(5):
-        m = matrix[i][counter_2]
-        matrix[i][counter_2] = matrix[i][counter_1]
-        matrix[i][counter_1] = m
-    counter_1 += 1
+        temp = matrix[i][verification]
+        matrix[i][verification] = matrix[i][counter]
+        matrix[i][counter] = temp
+    counter -= 1
 print("Original matrix:")   
 for i in range(5):
     for j in range(5):
