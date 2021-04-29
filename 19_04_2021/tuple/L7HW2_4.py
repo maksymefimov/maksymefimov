ee97c9984d05b5f2fc6f4131945d0bce79f8521d
maksymefimov <81595997+maksymefimov@users.noreply.github.com>
2021-04-29 13:22:47 +0300

    rebuild

--- 19_04_2021/tuple/L7HW2_4.py
+++ 19_04_2021/tuple/L7HW2_4.py
@@ -3,18 +3,23 @@
 ##ним знаходиться крапка. Друкує модифікований масив по сорок символів у рядку.
 sentence = list(input("Input a sentence: "))
 counter = 0
-for i in reversed(sentence):
-    if i  == '.':
-       counter = 1
-    if counter == 1: 
-        if i == '0' and sentence[sentence.index(i)-1] == 0:
-           print('1',index)
-           sentence.pop(sentence.index(i))
+flag = 0
+for i in reversed(range(0,len(sentence))):
+    if sentence[i]  == '.':
+        flag = 1
+    if sentence[i] == '0' and flag == 1 :
+        counter += 1
+    if ord(sentence[i]) in range(97,123) and flag == 1:
+        counter -= 1
+        index_slice = i+1
+        sentence = sentence[:index_slice] + sentence[index_slice + counter:]
+        flag = 0
+        counter = 0
+    elif sentence[i] in "123456789" and flag == 1:
+        flag = 0
+        counter = 0
+print(''.join(sentence))
         
-       
 
-print(sentence)
-        
-#недоделано
                           
    
